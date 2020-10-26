f527f6ac9792e0698d3e631febb98c5f767fae1f
Apoorva-Aithappa <71551341+Apoorva-Aithappa@users.noreply.github.com>
2020-10-27 00:01:08 +0530

    Update timer_simple.h

--- timer_simple.h
+++ timer_simple.h
@@ -16,10 +16,10 @@ void start_timer(void, int trigger_interval, (*callback_function)())
   NRF_TIMER1->TASKS_STOP = 1;	// Stop timer
   NRF_TIMER1->TASKS_CLEAR = 1; // Clear timer
   NRF_TIMER1->MODE = TIMER_MODE_MODE_Timer; // Setiing TIMER mode to timer --->0x504 offset
-  NRF_TIMER1->BITMODE = (TIMER_BITMODE_BITMODE_24Bit << TIMER_BITMODE_BITMODE_Pos); //setting the bitmode of the timer
+  NRF_TIMER1->BITMODE = (TIMER_BITMODE_BITMODE_24Bit << TIMER_BITMODE_BITMODE_Pos); //setting the bitmode of the timer--->0x508 offset
   
-  NRF_TIMER1->CC[0] = trigger_interval * 1000;  // meeting decimal requirement as the input is of millisecond resolution
-  NRF_TIMER2->INTENSET = (TIMER_INTENSET_COMPARE0_Enabled << TIMER_INTENSET_COMPARE0_Pos) | (TIMER_INTENSET_COMPARE1_Enabled << TIMER_INTENSET_COMPARE1_Pos); // recommonded to do as per some forum comments (for nrf52 series)
+  NRF_TIMER1->CC[0] = trigger_interval * 1000;  // meeting decimal requirement as the input is of millisecond resolution--->0x540 offset
+  NRF_TIMER1->INTENSET = (TIMER_INTENSET_COMPARE0_Enabled << TIMER_INTENSET_COMPARE0_Pos) | (TIMER_INTENSET_COMPARE1_Enabled << TIMER_INTENSET_COMPARE1_Pos); // recommonded to do as per some forum comments (for nrf52 series)--->0x304 offset
   attachInterrupt(TIMER1_IRQn, callback_function);    // setting up callback function
   NRF_TIMER1->TASKS_START = 1;	// Start TIMER
 }
